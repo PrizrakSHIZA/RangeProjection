44ad05ed7a69695a7131cbad309a497fe862aa62
PrizrakSHIZA <Prizrakshiza@gmail.com>
2023-08-25 17:08:39 +0300

    Faster code without variable in 1 row

--- RangeProjection.cs
+++ RangeProjection.cs
@@ -4,14 +4,12 @@ public class RangeProjection
 {
     public static float Project(float value, float min1, float max1, float min2, float max2)
     {
-        float percent = (value - min1) / (max1 - min1);
-        return (max2 * percent) + (min2 * (1 - percent));
+        return (max2 - min2) * ((value - min1) / (max1 - min1)) + min2;
     }
 
     public static float ProjectWithClamp(float value, float min1, float max1, float min2, float max2)
     {
         value = Mathf.Clamp(value, min1, max1);
-        float percent = (value - min1) / (max1 - min1);
-        return (max2 * percent) + (min2 * (1 - percent));
+        return (max2 - min2) * ((value - min1) / (max1 - min1)) + min2;
     }
 }
